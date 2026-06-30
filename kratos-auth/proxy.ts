34621c7b44e1a00f1319fa03afd1e244a81cf9a1
Cloner <clonerplus@yahoo.com>
2026-06-30 16:41:18 +0330

    changed kratos-auth app front flow

--- kratos-auth/proxy.ts
+++ kratos-auth/proxy.ts
@@ -15,7 +15,7 @@ function isPublicAuthRoute(pathname: string): boolean {
     )
 }
 
-export function proxy(request: NextRequest) {
+export function middleware(request: NextRequest) {
     if (isPublicAuthRoute(request.nextUrl.pathname)) {
         return NextResponse.next()
     }
