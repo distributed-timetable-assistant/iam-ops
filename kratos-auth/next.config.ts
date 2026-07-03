823ae324e2ceea417c748bf616df54fb9585ecad
cloner <clonerplus@yahoo.com>
2026-07-03 16:47:50 +0330

    NEXT_PUBLIC_ORY_SDK_URL hardcoded in next.config.ts

--- kratos-auth/next.config.ts
+++ kratos-auth/next.config.ts
@@ -2,6 +2,10 @@
 const nextConfig = {
     output: "standalone",
     allowedDevOrigins: ["127.0.0.1", "localhost"],
+    env: {
+        NEXT_PUBLIC_ORY_SDK_URL:
+            process.env.NEXT_PUBLIC_ORY_SDK_URL || "https://web.login.outi.ir",
+    },
     turbopack: {
         root: __dirname,
     },
